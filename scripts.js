436c4063e608abc55ae49bd041572d047364b81a
dagimawikebede8 <dagimawikebede8@gmail.com>
2026-04-21 01:58:41 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,97 +1,145 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
+const animeList = [
+  {
+    title: "Naruto",
+    genre: "Action",
+    rating: 9.5,
+    year: 2002,
+    image: "https://wallpapercave.com/wp/wp10059970.png"
+  },
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+  {
+    title: "One Piece",
+    genre: "Adventure",
+    rating: 9.5,
+    year: 1999,
+    image: "https://wallpapercave.com/wp/wp12922491.png"
+  },
 
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
+  {
+    title: "Attack on Titan",
+    genre: "Action",
+    rating: 10.0,
+    year: 2013,
+    image: "https://wallpapercave.com/wp/wp14351724.jpg"
+  },
+
+  {
+    title: "My Hero Academia",
+    genre: "Action",
+    rating: 8.3,
+    year: 2016,
+    image: "https://images.wallpapersden.com/image/download/my-hero-academia-season-7_bmdnZW6UmZqaraWkpJRnbm5nrWdraWs.jpg"
+  },
+
+  {
+    title: "Hunter x Hunter",
+    genre: "Adventure",
+    rating: 9.0,
+    year: 2011,
+    image: "https://wallpapercave.com/wp/wp12678227.jpg"
+  },
+
+  {
+    title: "Black Clover",
+    genre: "Fantasy",
+    rating: 8.2,
+    year: 2017,
+    image: "https://wallpapercave.com/wp/wp9148776.jpg"
+  },
+
+  {
+    title: "Demon Slayer",
+    genre: "Action",
+    rating: 8.6,
+    year: 2019,
+    image: "https://wallpapercave.com/wp/wp9566203.jpg"
+  },
+
+  {
+    title: "Jujutsu Kaisen",
+    genre: "Action",
+    rating: 8.7,
+    year: 2020,
+    image: "https://4kwallpapers.com/images/walls/thumbs_3t/14684.png"
+  },
+
+  {
+    title: "Fairy Tail",
+    genre: "Fantasy",
+    rating: 7.9,
+    year: 2009,
+    image: "https://i.pinimg.com/736x/cc/3b/b1/cc3bb10caaa1c5542c86ab89c33bf0a5.jpg"
+  },
+
+  {
+    title: "Death Note",
+    genre: "Thriller",
+    rating: 9.0,
+    year: 2006,
+    image: "https://i.pinimg.com/736x/67/31/b9/6731b95a00ef2cbdd75f8c346ccb6d24.jpg"
+  }
 ];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
 
-// This function adds cards the page to display the data in the array
+let currentAnimeList = animeList.slice();
+
 function showCards() {
-  const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = "";
-  const templateCard = document.querySelector(".card");
+  const container = document.getElementById("card-container");
+  const template = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
+  container.innerHTML = "";
 
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
+  for (let i = 0; i < currentAnimeList.length; i++) {
+    const anime = currentAnimeList[i];
+    const card = template.cloneNode(true);
 
-    const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
-    cardContainer.appendChild(nextCard); // Add new card to the container
+    card.style.display = "block";
+
+    card.querySelector("h2").textContent = anime.title;
+    card.querySelector("img").src = anime.image;
+
+    const items = card.querySelectorAll("li");
+    items[0].textContent = "Genre: " + anime.genre;
+    items[1].textContent = "Rating: " + anime.rating;
+    items[2].textContent = "Year: " + anime.year;
+
+    container.appendChild(card);
   }
 }
+function filterByGenre() {
+  const selectedGenre = document.getElementById("genre-filter").value;
 
-function editCardContent(card, newTitle, newImageURL) {
-  card.style.display = "block";
+  currentAnimeList = [];
 
-  const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
+  if (selectedGenre === "All") {
+    for (let i = 0; i < animeList.length; i++) {
+      currentAnimeList.push(animeList[i]);
+    }
+  } else {
+    for (let i = 0; i < animeList.length; i++) {
+      if (animeList[i].genre === selectedGenre) {
+        currentAnimeList.push(animeList[i]);
+      }
+    }
+  }
 
-  const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
-
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
+  sortByRating();
 }
 
-// This calls the addCards() function when the page is first loaded
+function sortByRating() {
+  const selectedSort = document.getElementById("rating-sort").value;
+
+  if (selectedSort === "high") {
+    currentAnimeList.sort(function (a, b) {
+      return b.rating - a.rating;
+    });
+  }
+
+  if (selectedSort === "low") {
+    currentAnimeList.sort(function (a, b) {
+      return a.rating - b.rating;
+    });
+  }
+
+  showCards();
+}
 document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!",
-  );
-}
-
-function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
-}
